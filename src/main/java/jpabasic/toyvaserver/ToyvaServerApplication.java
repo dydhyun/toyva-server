6ec60d39af512ef94b53ed5f0bcacd27ebff1937
dydhyun <dydgus625@naver.com>
2025-03-20 19:03:44 +0900

    first commit

---
+++ src/main/java/jpabasic/toyvaserver/ToyvaServerApplication.java
@@ -0,0 +1,13 @@
+package jpabasic.toyvaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyvaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyvaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jpabasic/toyvaserver/ToyvaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package jpabasic.toyvaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyvaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
