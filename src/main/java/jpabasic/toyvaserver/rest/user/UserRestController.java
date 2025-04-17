b154da817d1d5d253d56315c2ff674a1a45278bb
dydhyun <dydgus625@naver.com>
2025-04-17 23:16:07 +0900

    todo: 유저아이디 중복검사로 로그인로직만들기

--- src/main/java/jpabasic/toyvaserver/rest/user/UserRestController.java
+++ src/main/java/jpabasic/toyvaserver/rest/user/UserRestController.java
@@ -9,10 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,12 +19,36 @@ public class UserRestController {
     private static final Logger log = LoggerFactory.getLogger(UserRestController.class);
     private final UserService userService;
 
+    @GetMapping("/userId-check")
+    public ResponseEntity<?> userIdCheck(@RequestParam String userId){
+        ResponseDto<Boolean> responseDto = new ResponseDto<>();
+
+        try {
+            log.info("\nexec -- userIdCheck : {}", userId);
+            boolean isDuplicated = userService.userIdCheck(userId);
+
+            responseDto.setItem(isDuplicated);
+            responseDto.setStatusCode(HttpStatus.OK.value());
+            responseDto.setStatusMessage("userIdCheck ok");
+
+            return ResponseEntity.ok(responseDto);
+        }catch (Exception e){
+            log.error(e.getMessage());
+
+            responseDto.setErrorMessage(e.getMessage());
+            responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
+            responseDto.setStatusMessage(e.getMessage());
+
+            return ResponseEntity.internalServerError().body(responseDto);
+        }
+    }
+
     @PostMapping("/signup")
     public ResponseEntity<?> signup(@RequestBody UserDto userDto){
         ResponseDto<RegisteredUserDto> responseDto = new ResponseDto<>();
 
         try {
-            log.info("signup UserDto : {}", userDto.toString());
+            log.info("\nexec -- signup UserDto : {}", userDto.toString());
 
             RegisteredUserDto registeredUserDto = userService.register(userDto);
 
@@ -39,9 +60,9 @@ public class UserRestController {
         } catch (Exception e){
             log.error("signup err : {}", e.getMessage());
 
+            responseDto.setErrorMessage(e.getMessage());
             responseDto.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR.value());
             responseDto.setStatusMessage(e.getMessage());
-            responseDto.setErrorMessage(e.getMessage());
 
             return ResponseEntity.internalServerError().body(responseDto);
         }
@@ -52,7 +73,16 @@ public class UserRestController {
         ResponseDto<RegisteredUserDto> responseDto = new ResponseDto<>();
 
         try {
-            log.info("login user");
+            log.info("\nexec -- login user : {}", userDto);
+
+            boolean isExistUser = userService.userIdCheck(userDto.getUserId());
+
+            if (isExistUser){
+                log.info("존재하는유저임");
+            } else {
+                log.info("존재 안 하는유저임");
+            }
+            RegisteredUserDto registeredUserDto = userService.login(userDto);
 
             return ResponseEntity.ok(responseDto);
         } catch (Exception e){
