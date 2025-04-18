66949a1e6469036de4c39d15edda7d3bdbdb4ef3
dydhyun <dydgus625@naver.com>
2025-04-18 21:44:49 +0900

    add : message management

---
+++ src/main/java/jpabasic/toyvaserver/utils/MessageUtils.java
@@ -0,0 +1,11 @@
+package jpabasic.toyvaserver.utils;
+
+public class MessageUtils {
+
+    public static final String MSG_LOGIN_SUCCESS = "login accept";
+    public static final String MSG_USERID_DUPLICATION = "중복되는 아이디입니다.";
+    public static final String MSG_USERID_NOT_FOUND = "존재하지 않는 아이디입니다.";
+    public static final String MSG_USERPW_NOT_MATCH = "비밀번호가 일치하지 않습니다.";
+
+
+}
