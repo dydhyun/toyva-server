c15c2c9b84f26cd6c9d61aecbe61dd70fa3e830e
dydhyun <dydgus625@naver.com>
2025-04-18 21:45:45 +0900

    add : responseEntity management

---
+++ src/main/java/jpabasic/toyvaserver/utils/ResponseUtil.java
@@ -0,0 +1,25 @@
+package jpabasic.toyvaserver.utils;
+
+import jpabasic.toyvaserver.dto.ResponseDto;
+import org.springframework.http.ResponseEntity;
+
+public class ResponseUtil {
+    public static <T> ResponseEntity<ResponseDto<T>> ok(T data, int code, String message) {
+        ResponseDto<T> dto = new ResponseDto<>();
+        dto.setStatusCode(code);
+        dto.setStatusMessage(message);
+        dto.setItem(data);
+        
+        return ResponseEntity.ok(dto);
+    }
+
+    public static <T> ResponseEntity<ResponseDto<T>> error(T data, int code, String message) {
+        ResponseDto<T> dto = new ResponseDto<>();
+        dto.setStatusCode(code);
+        dto.setStatusMessage(message);
+        dto.setErrorMessage(message);
+        dto.setItem(data);
+        return ResponseEntity.status(code).body(dto);
+    }
+}
+
